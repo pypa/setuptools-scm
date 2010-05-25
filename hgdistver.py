f123ba1119a2bda0239a7919cf43297047018d98
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2010-05-25 16:21:40 +0200

    copy+test the archival data to version logic from setup.py of mercurial

--- hgdistver.py
+++ hgdistver.py
@@ -1,7 +1,19 @@
 
 
 
+
+def _archival_to_version(data):
+    "stolen logic from mercurials setup.py"
+    if 'tag' in data:
+        return data['tag']
+    elif 'latesttag' in data:
+        return '%(latesttag)s.dev%(latesttagdistance)s-%(node).12s' % data
+    else:
+        return data.get('node', '')[:12]
+
 def data_from_archival(path):
     import email
     data = email.message_from_file(open(str(path)))
     return dict(data.items())
+
+
--- test_hgdistver.py
+++ test_hgdistver.py
@@ -1,4 +1,6 @@
-from hgdistver import data_from_archival
+from hgdistver import \
+    data_from_archival, \
+    _archival_to_version
 
 def test_data_from_archival(tmpdir):
     tmpfile = tmpdir.join('test.archival')
@@ -10,3 +12,20 @@ def test_data_from_archival(tmpdir):
         'revision': '1',
     }
 
+
+archival_mapping = {
+    '1.0': {'tag': '1.0'},
+    '1.0.dev3-000000000000': {
+        'latesttag': '1.0',
+        'latesttagdistance': '3',
+        'node': '0'*20,
+    },
+    '0'*12: {
+        'node': '0'*20,
+    },
+
+}
+
+def test_archival_to_version():
+    for expected, data in archival_mapping.items():
+        assert _archival_to_version(data) == expected
