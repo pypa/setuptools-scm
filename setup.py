6c964295dbd7ae0b21066c81542b5ca6aea60585
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2010-05-25 17:25:34 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+import hgdistver
+setup(
+    name='hgdistver',
+    version=hgdistver.get_version(cachefile='hgdistver_version.py'),
+    author_mail='Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>',
+    description='utility lib to generate python package version infos from mercurial tags',
+    long_description=hgdistver.__doc__,
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Version Control',
+        'Topic :: System :: Software Distribution',
+        'Topic :: Utilities',
+    ])
+
+    
