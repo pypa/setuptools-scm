e142b132b5e72185d15c0866eb4a0a777fa41813
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2010-05-27 09:20:36 +0200

    add bitbucket url

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 import hgdistver
 setup(
     name='hgdistver',
+    url='http://bitbucket.org/RonnyPfannschmidt/hgdistver/',
     version=hgdistver.get_version(),
     author_email='Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>',
     description='utility lib to generate python package version infos from mercurial tags',
