09a10dfc1ffa19233dc676bee5ab6023ac02e05b
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2015-02-14 21:21:15 +0100

    enhanced test behaviour

--- test_functions.py
+++ test_functions.py
@@ -15,16 +15,16 @@ def test_next_tag(tag, expected):
 @pytest.mark.parametrize('tag, distance, dirty, current, guessed', [
     ('1.1', None, False, '1.1', '1.1'),
     ('1.1', 0, False, '1.1.post0+n00', '1.2.dev0+n00'),
-    ('1.1', 0, True, '1.1.post0+n00+time', '1.2.dev0+n00+time'),
+    ('1.1', 0, True, '1.1.post0+n00.time', '1.2.dev0+n00.time'),
     ('1.1', 3, False, '1.1.post3+n00', '1.2.dev3+n00'),
-    ('1.1', 3, True, '1.1.post3+n00+time', '1.2.dev3+n00+time'),
+    ('1.1', 3, True, '1.1.post3+n00.time', '1.2.dev3+n00.time'),
     pytest.mark.xfail(('1.1.dev', 3, False, '1.1.dev3', '1.1.dev3'),
                       reason='missed case'),
     ])
 @pytest.mark.parametrize('take_guess', [True, False])
 def test_format_version(take_guess, tag, distance, dirty,
                         current, guessed, monkeypatch):
-    monkeypatch.setattr(time, 'strftime', lambda x: '+time')
+    monkeypatch.setattr(time, 'strftime', lambda x: x[0] + 'time')
     version = dict(locals(), node='00')
     if take_guess:
         assert format_version(version, True) == guessed
