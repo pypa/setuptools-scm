27f0b6481675fcc07ec2ce09acbd2bd1ac8cf197
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2015-10-22 16:39:08 +0100

    Test 'python -m setuptools_scm' invocation (__main__.py).

---
+++ testing/test_main.py
@@ -0,0 +1,7 @@
+import os.path
+
+mainfile = os.path.join(
+    os.path.dirname(__file__), "..", "setuptools_scm", "__main__.py")
+with open(mainfile) as f:
+    code = compile(f.read(), "__main__.py", 'exec')
+    exec(code)
