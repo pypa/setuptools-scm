pypa/setuptools-scm

commit 0f1c98d032b421f65c8ef5b1042386a6b64bf0b6

restructure root finding, fixes #43 and #45
* PKG-INFO is no longer a scm, instead the version is substituted directly
* git root folder is verified
* simplicistic acceptance test for the root finding

setuptools_scm/integration.py
@@ -12,6 +12,8 @@ def version_keyword(dist, keyword, value):
         return
     if value is True:
         value = {}
+    if os.path.exists('PKG-INFO'):
+        value.pop('root', None)
     if getattr(value, '__call__', None):
         value = value()
     dist.metadata.version = get_version(**value)

.travis.yml
@@ -7,7 +7,7 @@ python:
 - '3.3'
 - '3.4'
 - '3.5'
-cache:  
+cache:
   files:
   - $HOME/.pip/cache
   - $Home/.cache/pip

setup.py
@@ -83,7 +83,6 @@ arguments = dict(
         .hg = setuptools_scm.hg:parse
         .git = setuptools_scm.git:parse
         .hg_archival.txt = setuptools_scm.hg:parse_archival
-        PKG-INFO = setuptools_scm.hacks:parse_pkginfo
 
         [setuptools_scm.files_command]
         .hg = setuptools_scm.hg:FILES_COMMAND

setuptools_scm/git.py
@@ -1,10 +1,16 @@
-from .utils import do, do_ex
+from .utils import do, do_ex, trace
 from .version import meta
+from os.path import abspath, realpath
+
 
 FILES_COMMAND = 'git ls-files'
 
 
 def parse(root):
+    real_root, _, ret = do_ex('git rev-parse --show-toplevel', root)
+    trace('real root', real_root)
+    if abspath(realpath(real_root)) != abspath(realpath(root)):
+        return
     rev_node, _, ret = do_ex('git rev-parse --verify --quiet HEAD', root)
     if ret:
         return meta('0.0')

setuptools_scm/utils.py
@@ -47,7 +47,7 @@ def do_ex(cmd, cwd='.'):
         shlex.split(cmd),
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
-        cwd=cwd,
+        cwd=str(cwd),
         env=_always_strings(dict(
             os.environ,
             # disable hgrc processing other than .hg/hgrc

testing/test_basic_api.py
@@ -33,7 +33,7 @@ class Wd(object):
         self.cwd = cwd
 
     def __call__(self, cmd):
-        do(cmd, str(self.cwd))
+        return do(cmd, self.cwd)
 
     def write(self, name, value):
         filename = self.cwd.join(name)

testing/test_regressions.py
@@ -1,0 +1,23 @@
+from setuptools_scm.utils import do_ex, do
+def test_pkginfo_noscmroot(tmpdir, monkeypatch):
+    """if we are indeed a sdist, the root does not apply"""
+    monkeypatch.delenv("SETUPTOOLS_SCM_DEBUG")
+
+    # we should get the version from pkg-info if git is broken
+    p = tmpdir.ensure('sub/package', dir=1)
+    tmpdir.mkdir('.git')
+    p.join('setup.py').write(
+        'from setuptools import setup;'
+        'setup(use_scm_version={"root": ".."})')
+
+    _, stderr, exit = do_ex('python setup.py --version', p)
+    assert 'setuptools-scm was unable to detect version for' in stderr
+    assert exit == 1
+
+    p.join("PKG-INFO").write('Version: 1.0')
+    res = do('python setup.py --version', p)
+    assert res == '1.0'
+
+    do('git init', p.dirpath())
+    res = do('python setup.py --version', p)
+    assert res == '1.0'

commit ff948dcd9973a12a139012d9edc13e572471705f

fix bug when using callable version_scheme
This fixes bug introduced with 0f1c98d032b421f65c8ef5b1042386a6b64bf0b6,
looking like this:

    Complete output from command python setup.py egg_info:
    Traceback (most recent call last):
      File "<string>", line 1, in <module>
      File "/tmp/pip-build-zfpnaarq/xd-docker/setup.py", line 69, in <module>
        setup_requires=['setuptools_scm'],
      File "/usr/local/lib/python3.4/distutils/core.py", line 108, in setup
        _setup_distribution = dist = klass(attrs)
      File "/usr/local/lib/python3.4/site-packages/setuptools/dist.py", line 272, in __init__
        _Distribution.__init__(self,attrs)
      File "/usr/local/lib/python3.4/distutils/dist.py", line 280, in __init__
        self.finalize_options()
      File "/usr/local/lib/python3.4/site-packages/setuptools/dist.py", line 327, in finalize_options
        ep.load()(self, ep.name, value)
      File "/usr/local/lib/python3.4/site-packages/setuptools_scm-1.10.2.dev8+ng4835e4d-py3.4.egg/setuptools_scm/integration.py", line 16, in version_keyword
    AttributeError: 'function' object has no attribute 'pop'

Signed-off-by: Esben Haabendal <esben@haabendal.dk>

setuptools_scm/integration.py
@@ -12,10 +12,10 @@ def version_keyword(dist, keyword, value):
         return
     if value is True:
         value = {}
-    if os.path.exists('PKG-INFO'):
-        value.pop('root', None)
     if getattr(value, '__call__', None):
         value = value()
+    if os.path.exists('PKG-INFO'):
+        value.pop('root', None)
     dist.metadata.version = get_version(**value)
 
 