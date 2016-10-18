433af1c0d190db400fc54b458f16db3e5227afca
cclauss <cclauss@bluewin.ch>
2016-10-18 18:52:12 +0100

    Update setup.py
    Python 3.4 --> Python 3.5 to match https://github.com/pypa/setuptools_scm/blob/master/.travis.yml#L4-L8
    
    Related to https://github.com/pypa/setuptools_scm/issues/112 but NOT the fix for https://github.com/pypa/setuptools_scm/issues/112 .

--- setup.py
+++ setup.py
@@ -110,7 +110,7 @@ arguments = dict(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Version Control',
         'Topic :: System :: Software Distribution',
