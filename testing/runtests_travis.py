bac6f0b4dec7564c98fc36fb55bcec85257d4814
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2017-01-29 20:57:07 +0100

    add extra comments to the travis test script

--- testing/runtests_travis.py
+++ testing/runtests_travis.py
@@ -4,9 +4,12 @@ from subprocess import call
 import os
 
 if os.environ.get('TOXENV'):
+    # normal tox run, lets jsut have tox do its job
     import tox
     tox.cmdline()
 elif os.environ.get('SELFINSTALL'):
+    # self install testing needs some clarity
+    # so its being executed without any other tools running
     call('python setup.py sdist', shell=True)
     call('easy_install dist/*', shell=True)
     import pkg_resources
