70224f5a202ece43bcd2010f8e299654242b02a2
Nicholas Nadeau, P.Eng., AVS <nnadeau@users.noreply.github.com>
2018-01-01 16:37:27 -0500

    added python_requires

--- setup.py
+++ setup.py
@@ -108,6 +108,7 @@ arguments = dict(
         'Topic :: System :: Software Distribution',
         'Topic :: Utilities',
     ],
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
 )
 
 if __name__ == '__main__':
