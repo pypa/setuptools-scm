29296ffa7ed351a0f1188236b07e8dbce4805387
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2018-04-25 18:57:59 +0200

    Adapt git_file_finder tests style

--- testing/test_git_file_finder.py
+++ testing/test_git_file_finder.py
@@ -3,36 +3,31 @@
 # License LGPLv3 (http://www.gnu.org/licenses/lgpl-3.0-standalone.html)
 
 import os
-import subprocess
 import sys
 
-from pathlib2 import Path
 import pytest
 
 from setuptools_scm.git_file_finder import find_files
 
 
 @pytest.fixture
-def ingitdir(tmpdir):
-    cwd = os.getcwd()
-    os.chdir(str(tmpdir))
-    try:
-        subprocess.check_call(['git', 'init'])
-        Path('file1').touch()
-        adir = Path('adir')
-        adir.mkdir()
-        (adir / 'filea').touch()
-        bdir = Path('bdir')
-        bdir.mkdir()
-        (bdir / 'fileb').touch()
-        subprocess.check_call(['git', 'add', '.'])
-        subprocess.check_call(['git', 'commit', '-m', '...'])
-        yield
-    finally:
-        os.chdir(cwd)
+def inwd(wd):
+    wd('git init')
+    wd('git config user.email test@example.com')
+    wd('git config user.name "a test"')
+    wd.add_command = 'git add .'
+    wd.commit_command = 'git commit -m test-{reason}'
+    (wd.cwd / 'file1').ensure(file=True)
+    adir = (wd.cwd / 'adir').ensure(dir=True)
+    (adir / 'filea').ensure(file=True)
+    bdir = (wd.cwd / 'bdir').ensure(dir=True)
+    (bdir / 'fileb').ensure(file=True)
+    wd.add_and_commit()
+    with wd.cwd.as_cwd():
+        yield wd
 
 
-def test_basic(ingitdir):
+def test_basic(inwd):
     assert set(find_files()) == {
         'file1',
         'adir/filea',
@@ -48,11 +43,9 @@ def test_basic(ingitdir):
     }
 
 
-def test_symlink_dir(ingitdir):
-    (Path('adir') / 'bdirlink').\
-        symlink_to('../bdir', target_is_directory=True)
-    subprocess.check_call(['git', 'add', '.'])
-    subprocess.check_call(['git', 'commit', '-m', '...'])
+def test_symlink_dir(inwd):
+    (inwd.cwd / 'adir' / 'bdirlink').mksymlinkto('../bdir')
+    inwd.add_and_commit()
     assert set(find_files('adir')) == {
         'adir/filea',
         'adir/bdirlink/fileb',
@@ -61,32 +54,27 @@ def test_symlink_dir(ingitdir):
 
 @pytest.mark.skipif(sys.platform == 'win32',
                     reason="symlinks to files not supported on windows")
-def test_symlink_file(ingitdir):
-    (Path('adir') / 'file1link').\
-        symlink_to('../file1', target_is_directory=False)
-    subprocess.check_call(['git', 'add', '.'])
-    subprocess.check_call(['git', 'commit', '-m', '...'])
+def test_symlink_file(inwd):
+    (inwd.cwd / 'adir' / 'file1link').mksymlinkto('../file1')
+    inwd.add_and_commit()
     assert set(find_files('adir')) == {
         'adir/filea',
         'adir/file1link',
     }
 
 
-def test_symlink_loop(ingitdir):
-    (Path('adir') / 'loop').\
-        symlink_to('../adir', target_is_directory=True)
-    subprocess.check_call(['git', 'add', '.'])
-    subprocess.check_call(['git', 'commit', '-m', '...'])
+def test_symlink_loop(inwd):
+    (inwd.cwd / 'adir' / 'loop').mksymlinkto('../adir')
+    inwd.add_and_commit()
     assert set(find_files('adir')) == {
         'adir/filea',
     }
 
 
-def test_symlink_dir_out_of_git(ingitdir):
-    (Path('adir') / 'outsidedirlink').\
-        symlink_to(Path(__file__) / '..', target_is_directory=True)
-    subprocess.check_call(['git', 'add', '.'])
-    subprocess.check_call(['git', 'commit', '-m', '...'])
+def test_symlink_dir_out_of_git(inwd):
+    (inwd.cwd / 'adir' / 'outsidedirlink').\
+        mksymlinkto(os.path.join(__file__, '..'))
+    inwd.add_and_commit()
     assert set(find_files('adir')) == {
         'adir/filea',
     }
@@ -94,33 +82,29 @@ def test_symlink_dir_out_of_git(ingitdir):
 
 @pytest.mark.skipif(sys.platform == 'win32',
                     reason="symlinks to files not supported on windows")
-def test_symlink_file_out_of_git(ingitdir):
-    (Path('adir') / 'outsidefilelink').\
-        symlink_to(__file__, target_is_directory=False)
-    subprocess.check_call(['git', 'add', '.'])
-    subprocess.check_call(['git', 'commit', '-m', '...'])
+def test_symlink_file_out_of_git(inwd):
+    (inwd.cwd / 'adir' / 'outsidefilelink').mksymlinkto(__file__)
+    inwd.add_and_commit()
     assert set(find_files('adir')) == {
         'adir/filea',
     }
 
 
-def test_empty_root(ingitdir):
-    subdir = Path('cdir') / 'subdir'
-    subdir.mkdir(parents=True)
-    (subdir / 'filec').touch()
-    subprocess.check_call(['git', 'add', '.'])
-    subprocess.check_call(['git', 'commit', '-m', '...'])
+def test_empty_root(inwd):
+    subdir = inwd.cwd / 'cdir' / 'subdir'
+    subdir.ensure(dir=True)
+    (subdir / 'filec').ensure(file=True)
+    inwd.add_and_commit()
     assert set(find_files('cdir')) == {
         'cdir/subdir/filec',
     }
 
 
-def test_empty_subdir(ingitdir):
-    subdir = Path('adir') / 'emptysubdir' / 'subdir'
-    subdir.mkdir(parents=True)
-    (subdir / 'xfile').touch()
-    subprocess.check_call(['git', 'add', '.'])
-    subprocess.check_call(['git', 'commit', '-m', '...'])
+def test_empty_subdir(inwd):
+    subdir = inwd.cwd / 'adir' / 'emptysubdir' / 'subdir'
+    subdir.ensure(dir=True)
+    (subdir / 'xfile').ensure(file=True)
+    inwd.add_and_commit()
     assert set(find_files('adir')) == {
         'adir/filea',
         'adir/emptysubdir/subdir/xfile',
