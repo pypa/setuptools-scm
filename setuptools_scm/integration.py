b549c0c6bd31031a05902a6c1fc76972072734c9
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2018-04-28 18:38:52 +0200

    Rework file finder integration
    - do not replace '' with '.', fixes #248
    - do not assume path is at scm toplevel, fixes #251

--- setuptools_scm/integration.py
+++ setuptools_scm/integration.py
@@ -1,4 +1,4 @@
-import os
+from pkg_resources import iter_entry_points
 
 from .version import _warn_if_setuptools_outdated
 from .utils import do
@@ -22,22 +22,14 @@ def version_keyword(dist, keyword, value):
     dist.metadata.version = get_version(**value)
 
 
-def find_files(path='.'):
-    if not path:
-        path = '.'
-    abs = os.path.abspath(path)
-    ep = next(iter_matching_entrypoints(
-        abs, 'setuptools_scm.files_command'), None)
-    if ep:
+def find_files(path=''):
+    for ep in iter_entry_points('setuptools_scm.files_command'):
         command = ep.load()
-        try:
-            if isinstance(command, str):
-                return do(ep.load(), path).splitlines()
-            else:
-                return command(path)
-        except Exception:
-            print("File Finder Failed for %s" % ep)
-            raise
-
-    else:
-        return []
+        if isinstance(command, str):
+            # this technique is deprecated
+            res = do(ep.load(), path or '.').splitlines()
+        else:
+            res = command(path)
+        if res:
+            return res
+    return []
