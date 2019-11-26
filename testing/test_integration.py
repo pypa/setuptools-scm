24c24ab8a6c662079c397a8d91228b3b8d45f033
layday <layday@protonmail.com>
2019-11-27 00:23:29 +0200

    Test pyproject.toml integration using git
    See #374.

--- testing/test_integration.py
+++ testing/test_integration.py
@@ -1,6 +1,18 @@
 import sys
 
+import pytest
+
 from setuptools_scm.utils import do
+
+
+@pytest.fixture
+def wd(wd):
+    wd("git init")
+    wd("git config user.email test@example.com")
+    wd('git config user.name "a test"')
+    wd.add_command = "git add ."
+    wd.commit_command = "git commit -m test-{reason}"
+    return wd
 
 
 def test_pyproject_support(tmpdir, monkeypatch):
@@ -14,3 +26,15 @@ fallback_version = "12.34"
     pkg.join("setup.py").write("__import__('setuptools').setup()")
     res = do((sys.executable, "setup.py", "--version"), pkg)
     assert res == "12.34"
+
+
+@pytest.mark.xfail
+def test_pyproject_support_with_git(tmpdir, monkeypatch, wd):
+    monkeypatch.delenv("SETUPTOOLS_SCM_DEBUG")
+    pkg = tmpdir.join("wd")
+    pkg.join("pyproject.toml").write(
+        """[tool.setuptools_scm]"""
+    )
+    pkg.join("setup.py").write("__import__('setuptools').setup()")
+    res = do((sys.executable, "setup.py", "--version"), pkg)
+    assert res == "0.1.dev0"
