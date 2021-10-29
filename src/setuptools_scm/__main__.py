fc3dd17b332a76986da00d03acbb80da760bebb8
Anderson Bravalheri <andersonbravalheri@gmail.com>
2021-10-29 19:30:19 +0100

    Consider configuration when running `__main__`
    Since the usage with `setup.py` is deprecated, it would be nice to have
    a command that give the same results as:
    
        python setup.py --version
    
    The idea of the changes implemented here is to do that using the
    `__main__` module.
    
    The methodology consists in using the `find_files` function to look for
    a `pyproject.toml` file and then reading the configuration from there.
    
    If `pyproject.toml` file is not found or it does not contain a
    `[tool.setuptools_scm]` table, the command works as implemented before
    (guessing the next version).

--- src/setuptools_scm/__main__.py
+++ src/setuptools_scm/__main__.py
@@ -1,13 +1,21 @@
 import sys
 
+from setuptools_scm import _get_version
 from setuptools_scm import get_version
+from setuptools_scm.config import Configuration
 from setuptools_scm.integration import find_files
 
 
 def main() -> None:
-    print("Guessed Version", get_version())
+    files = list(sorted(find_files("."), key=len))
+    try:
+        pyproject = next(fname for fname in files if fname.endswith("pyproject.toml"))
+        print(_get_version(Configuration.from_file(pyproject)))
+    except (LookupError, StopIteration):
+        print("Guessed Version", get_version())
+
     if "ls" in sys.argv:
-        for fname in find_files("."):
+        for fname in files:
             print(fname)
 
 
--- testing/test_main.py
+++ testing/test_main.py
@@ -1,4 +1,8 @@
 import os.path
+import sys
+import textwrap
+
+import pytest
 
 
 def test_main():
@@ -8,3 +12,40 @@ def test_main():
     with open(mainfile) as f:
         code = compile(f.read(), "__main__.py", "exec")
         exec(code)
+
+
+@pytest.fixture
+def repo(wd):
+    wd("git init")
+    wd("git config user.email user@host")
+    wd("git config user.name user")
+    wd.add_command = "git add ."
+    wd.commit_command = "git commit -m test-{reason}"
+
+    wd.write("README.rst", "My example")
+    wd.add_and_commit()
+    wd("git tag v0.1.0")
+
+    wd.write("file.txt", "file.txt")
+    wd.add_and_commit()
+
+    return wd
+
+
+def test_repo_with_config(repo):
+    pyproject = """\
+    [tool.setuptools_scm]
+    version_scheme = "no-guess-dev"
+
+    [project]
+    name = "example"
+    """
+    repo.write("pyproject.toml", textwrap.dedent(pyproject))
+    repo.add_and_commit()
+    res = repo((sys.executable, "-m", "setuptools_scm"))
+    assert res.startswith("0.1.0.post1.dev2")
+
+
+def test_repo_without_config(repo):
+    res = repo((sys.executable, "-m", "setuptools_scm"))
+    assert res.startswith("Guessed Version 0.1.1.dev1")
