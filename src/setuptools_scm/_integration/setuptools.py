7c4c8ae45127c36f6f5ea125d3f7ce6cd0d26f20
Jason R. Coombs <jaraco@jaraco.com>
2022-08-19 13:09:54 -0400

    Remove IO parameter for read_dist_name_from_setup_cfg, which is unused and untested.

--- src/setuptools_scm/_integration/setuptools.py
+++ src/setuptools_scm/_integration/setuptools.py
@@ -1,11 +1,10 @@
 from __future__ import annotations
 
 import os
-from typing import IO
 
 
 def read_dist_name_from_setup_cfg(
-    input: str | os.PathLike[str] | IO[str] = "setup.cfg",
+    input: str | os.PathLike[str] = "setup.cfg",
 ) -> str | None:
 
     # minimal effort to read dist_name off setup.cfg metadata
@@ -13,10 +12,7 @@ def read_dist_name_from_setup_cfg(
 
     parser = configparser.ConfigParser()
 
-    if isinstance(input, (os.PathLike, str)):
-        parser.read([input])
-    else:
-        parser.read_file(input)
+    parser.read([input])
 
     dist_name = parser.get("metadata", "name", fallback=None)
     return dist_name
