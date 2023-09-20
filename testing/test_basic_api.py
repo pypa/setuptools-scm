597ba88f8b8fc5e990815212eddc2e2b1f55b1d3
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2023-09-20 13:33:38 +0200

    Apply suggestions from code review - pep8 compliance
    Co-authored-by: Jonathan de Bruin <jonathandebruinos@gmail.com>

--- testing/test_basic_api.py
+++ testing/test_basic_api.py
@@ -180,9 +180,9 @@ def test_dump_version(tmp_path: Path) -> None:
     dump_version(tmp_path, version, "first.py", scm_version=scm_version)
     lines = read("first.py").splitlines()
     assert lines[-2:] == [
-        "__version__ = version = '1.0.dev42' # type: str",
+        "__version__ = version = '1.0.dev42'  # type: str",
         "__version_tuple__ = version_tuple = (1, 0, 'dev42')"
-        " # type: tuple[int | str, ...]",
+        "  # type: tuple[int | str, ...]",
     ]
 
     version = "1.0.1+g4ac9d2c"
